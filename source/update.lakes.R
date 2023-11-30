5ddfe82edee6b375d6bcb027d5df3c3140a81d4f
Ashley Trudeau <ashley.trudeau@gmail.com>
2023-11-30 14:40:44 -0600

    update.lakes now works with multiple lakes. now test loops

--- source/update.lakes.R
+++ source/update.lakes.R
@@ -7,26 +7,36 @@ update.lakes<-function(y, fishery, parameters){
   if(y<nYears){
     
     lakeStatus<-fishery[["lakeStatus"]]
-    fishPop<-fishery[["fishPop"]]
-    startPop<-fishery[["startPop"]]
+    fishPops<-fishery[["fishPops"]]
+    startPops<-fishery[["startPops"]]
     
-    # update lakeStatus
+    # update lakeStatus for day 1 of the next year
     
     thisYear<-annualOutput[annualOutput$year==y,]
     
     nextYear.day1<-lakeStatus[lakeStatus$year==y+1 & lakeStatus$day==1,]
     
     nextYear.day1$fishN<-thisYear$fishNEnd
-   # nextYear.day1$fishB<-thisYear$fishBEnd
-    
+
     lakeStatus[lakeStatus$year==y+1 & lakeStatus$day==1,]<-nextYear.day1
     
     fishery[["lakeStatus"]]<-lakeStatus
     
-    # update startPop with aged fish
+    # update startPops with aged fish
     
-    startPop[,y+1]<-fishPop[,y+2]
-    fishery[["startPop"]]<-startPop
+    fishPops.to.move<-lapply(fishPops, function(x) x[,y+2])
+    
+    startPops<-lapply(seq_along(startPops), function(x){
+      lake_matrix<-startPops[[x]]
+      lake_name<-names(startPops)[x]
+      lake_vector<-fishPops.to.move[[lake_name]]
+      
+      lake_matrix[,y+1]<-lake_vector
+      return(lake_matrix)
+    })
+    
+
+    fishery[["startPops"]]<-startPops
     
 
  
