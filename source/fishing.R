7c8dc5153377538017678126b5b02978df211388
Ashley Trudeau <ashley.trudeau@gmail.com>
2024-02-28 09:12:03 -0600

    vectorized fishing(); seems to be working, a nd much faster

--- source/fishing.R
+++ source/fishing.R
@@ -21,20 +21,18 @@ fishing<-function(fishery, parameters, t, y){
   q<-parameters[["q"]]
   nAnglers<-parameters[["nAnglers"]]
   nLakes<-parameters[["nLakes"]]
+  nAges<-parameters[["nAges"]]
   
   # pull fish population matrix for this year. (y)
 
   fishPopYear<-lapply(fishPops, function(x) x[,y])
   
-  # pull harvestAge vector for this year (it's blank)
-  harvestAgeYear<-lapply(harvestAge, function(x) x[,y])
 
 
-  # function loops through anglers to calculate their catch (age specific)
-  # I'm replacing the loop with a (hopefully faster) vectorized version
+  # I've replaced the loop with a (hopefully faster) vectorized version
   
-# solution from here https://stackoverflow.com/questions/46983716/does-a-multi-value-purrrpluck-exist 
-  # I have no idea how this funciton works, but it plucked multiple items from the fishPopYear list corresponding to 
+# pluck_multiple() solution from here https://stackoverflow.com/questions/46983716/does-a-multi-value-purrrpluck-exist 
+  # I'm not totally sure how this function works, but it plucked multiple items from the fishPopYear list corresponding to 
   # WBICs chosen
   
   pluck_multiple <- function(x, ...) {
@@ -66,7 +64,7 @@ fishing<-function(fishery, parameters, t, y){
   # this turns the catchAgeLake.round list (which has catch of each age class) into a dataframe.
   # This sums up all catch/harvest by lake and age, which can then be split back into a list
   stackCatchAgeLake<-stack(catchAgeLake.round)%>%
-    mutate(age=rep(0:15, nAnglers))%>%
+    mutate(age=rep(0:nAges, nAnglers))%>%
     rename("wbic"=ind,
            "catch"=values)%>%
     group_by(wbic, age)%>%
@@ -77,7 +75,7 @@ fishing<-function(fishery, parameters, t, y){
   # the list is incomplete because it only has lakes that were visited by anglers (in anglerDecision matrix)
   splitCatchAgeLake<-split(stackCatchAgeLake, stackCatchAgeLake$wbic)
   splitCatchAgeLake<-lapply(splitCatchAgeLake, subset, select=-wbic)
-  # this turns total catch back into a v ector
+  # this turns total catch back into a vector
   splitCatchAgeLake<-lapply(splitCatchAgeLake, deframe)
   
   # for the rest of the lakes that were unvisited, I'm making blank list elements to append to splitCatchAgeLake
@@ -94,121 +92,18 @@ fishing<-function(fishery, parameters, t, y){
   harvestAgeYear<-catchAgeLakeAppend[names(fishPopYear)]
    #  now I can subtract harvestAgeYear from fishPopYear to get new fish populations
   
-  # -------------------------------------
-  # adding splitCatchAge to allLakesList to make harvestAgeYear
-
-  # this is that aggregation step: 
-  fishCaughtWBIC<-pluck_multiple(catchAgeLake.round, rep(filterCatch$WBIC, filterCatch$n))
-  stack<-stack(fishCaughtWBIC)
-  stack$age<-rep(0:15, nrow(stack)/16)
-  catchAgeWBIC<-stack%>%
-    rename("WBIC"=ind,
-           "catch"=values)%>%
-    group_by(WBIC, age)%>%
-    mutate(catch=sum(catch))
+  newFishPopYear<-mapply(`-`, fishPopYear, harvestAgeYear, SIMPLIFY=F)
   
-  catchByAge<-split(catchAgeWBIC, catchAgeWBIC$WBIC)
-  
-  # to make harvestAgeYear, add aggregate age and lake specific catch to allLakesList
-  
-  
-  
-  # I also want to fill harvestAgeYear with age specific harvest from lakes
-  
-  # These are lakes where harvest took place
-  filterCatch<-anglerDecisions%>%
-    group_by(WBIC)%>%
-    summarize(nHarvested=sum(harvest), 
-              n=n())%>%
-    ungroup()%>%
-    filter(nHarvested!=0)
-  
-  # remove age-specific catch from the population
-  
-  #  first step, need to aggregate total catch by age for each lake chosen
-  # simplest way may be to filter catchAgeLake.round to only lakes where fish were caught
- fishCaughtWBIC<-pluck_multiple(catchAgeLake.round, rep(filterCatch$WBIC, filterCatch$n))
-  stack<-stack(fishCaughtWBIC)
-  stack$age<-rep(0:15, nrow(stack)/16)
-  catchAgeWBIC<-stack%>%
-    rename("WBIC"=ind,
-           "catch"=values)%>%
-    group_by(WBIC, age)%>%
-    mutate(catch=sum(catch))
-  #  this has only lakes where >0 f ish were caught
-  catchByAge<-split(catchAgeWBIC, catchAgeWBIC$WBIC)
-  
-  # now make full list of catch by age
-  
-  allLakesList<-vector("list", length(catchAgeLake.round))
-  names(allLakesList)<-names(catchAgeLake.round)
-  
-  # and fill in catch at age
-  
-
-  # try unlisting, group_by, sum, then making it back into a list
-  # this is where I left off 2/27
-  
-# this below didn't w ork because I need catch at age  
-  # unlistCatchCol<-unlist(catchCol)
-  # unlistCatchCol.df<-data.frame(WBIC=names(unlistCatchCol), catch=unname(unlistCatchCol))%>%
-  #   group_by(WBIC)%>%
-  #   summarize(catch=sum(catch))
-  # 
-  # aggCatchLake<-data.frame(WBIC=names(fishPopYear))%>%
-  #   left_join(unlistCatchCol.df, by="WBIC")%>%
-  #   mutate(catch=ifelse(is.na(catch), 0, catch))
-  # 
-  # catchColList<-split(aggCatchLake, aggCatchLake$WBIC)
-  # aggCatchLake<-catchColList[names(fishPopYear)]
-  # 
-  # # now subtract harvest from fishPopYear
-  # aggCatchLake2<-lapply(aggCatchLake, "[", "catch")
-  # 
-  # 
-  # newFishPopYear<-mapply(`-` , fishPopYear, aggCatchLake2)
-  
-  # update the fishPopYear and harvestAge matrix
-  
-  for(n in 1:nAnglers){
-    n<-1
-    # pull angler's chosen lake
-    WBIC<-anglerDecisions[n,]$WBIC
-    # pull population vector for that lake
-    popAgeLake<-fishPopYear[[WBIC]]
-    harvestAgeLake<-harvestAgeYear[[WBIC]]
-    # calculate age-specific catch
-    catchAgeLake<-round((q*selectivity*popAgeLake^beta)*4)
-    # this is a problem
-    # update anglerDecisions df with aggregate catch
-    anglerDecisions[n,]$catch<-sum(catchAgeLake)
-    # for now harvest is the same as catch
-    anglerDecisions[n,]$harvest<-sum(catchAgeLake)
-    
-    # remove catch from the population
-    popAgeLake<-popAgeLake-catchAgeLake
-    harvestAgeLake<-harvestAgeLake+catchAgeLake
-    
-    # update the fishPopYear and harvestAge matrix
-    
-    fishPopYear[[WBIC]]<-popAgeLake
-    harvestAgeYear[[WBIC]]<-harvestAgeLake
-
-  }
-  
-  # put harvestAgeYear and fishPopYear back into harvestAge and fishPops. (this 'yearly' column updates daily as 
-  # catch and harvest continue)
-
 
   for(i in 1:length(fishPops)){
-  fishPops[[i]][,y]<-fishPopYear[[i]]
+  fishPops[[i]][,y]<-newFishPopYear[[i]]
   harvestAge[[i]][,y]<-harvestAgeYear[[i]]
   }
   
 
   
   # now update lakeStatus
-  # I don't have an object to track total catch; I may need to add one later if 
+  # I don't have an object to track total individual catch; I may need to add one later if 
   # individual catch rates (separate from harvest) are important for angler decisions.
   
   totalHarvest<-sapply(harvestAgeYear, sum)
